9112ca3332030668b866eb99ed63eb77c48bb67b
Jim Clark <jim.clark@generalassemb.ly>
2022-06-02 11:37:34 -0700

    Finish useEffect...

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -4,7 +4,6 @@ import { getUser } from '../../utilities/users-service';
 import AuthPage from '../AuthPage/AuthPage';
 import NewOrderPage from '../NewOrderPage/NewOrderPage';
 import OrderHistoryPage from '../OrderHistoryPage/OrderHistoryPage';
-import NavBar from '../../components/NavBar/NavBar';
 import './App.css';
 
 function App() {
@@ -13,14 +12,11 @@ function App() {
   return (
     <main className="App">
       { user ?
-        <>
-          <NavBar user={user} setUser={setUser} />
-          <Routes>
-            {/* Route components in here */}
-            <Route path="/orders/new" element={<NewOrderPage />} />
-            <Route path="/orders" element={<OrderHistoryPage />} />
-          </Routes>
-        </>
+        <Routes>
+          {/* Route components in here */}
+          <Route path="/orders/new" element={<NewOrderPage user={user} setUser={setUser} />} />
+          <Route path="/orders" element={<OrderHistoryPage />} />
+        </Routes>
         :
         <AuthPage setUser={setUser} />
       }
--- src/pages/NewOrderPage/NewOrderPage.jsx
+++ src/pages/NewOrderPage/NewOrderPage.jsx
@@ -1,10 +1,18 @@
 import { useState, useEffect, useRef } from 'react';
 import * as itemsAPI from '../../utilities/items-api';
+import './NewOrderPage.css';
+import { Link } from 'react-router-dom';
+import Logo from '../../components/Logo/Logo';
+import MenuList from '../../components/MenuList/MenuList';
+import CategoryList from '../../components/CategoryList/CategoryList';
+import OrderDetail from '../../components/OrderDetail/OrderDetail';
+import UserLogOut from '../../components/UserLogOut/UserLogOut';
 
-export default function NewOrderPage() {
+export default function NewOrderPage({ user, setUser }) {
   // If your state will ultimately be an array, ALWAYS
   // initialize to an empty array
   const [menuItems, setMenuItems] = useState([]);
+  const [activeCat, setActiveCat] = useState('');
   const categoriesRef = useRef([]);
 
   useEffect(function() {
@@ -12,6 +20,7 @@ export default function NewOrderPage() {
       const items = await itemsAPI.getAll();
       categoriesRef.current = [...new Set(items.map(item => item.category.name))];
       setMenuItems(items);
+      setActiveCat(categoriesRef.current[0]);
     }
     getItems();
   }, []);
@@ -19,9 +28,21 @@ export default function NewOrderPage() {
   // function running ONLY after the FIRST render
   
   return (
-    <>
-    <h1>NewOrderPage</h1>
-    <button onClick={() => setMenuItems(Date.now())}>Trigger re-render</button>
-    </>
+    <main className="NewOrderPage">
+      <aside>
+        <Logo />
+        <CategoryList
+          categories={categoriesRef.current}
+          activeCat={activeCat}
+          setActiveCat={setActiveCat}
+        />
+        <Link to="/orders" className="button btn-sm">PREVIOUS ORDERS</Link>
+        <UserLogOut user={user} setUser={setUser} />
+      </aside>
+      <MenuList
+        menuItems={menuItems.filter(item => item.category.name === activeCat)}
+      />
+      <OrderDetail />
+    </main>
   );
 }
