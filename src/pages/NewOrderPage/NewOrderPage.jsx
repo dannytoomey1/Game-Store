ce4f0a1a0ce3724ea9be4ee2750585a5fbfe2541
Jim Clark <jim.clark@generalassemb.ly>
2022-06-02 11:03:17 -0700

    Add items-api

--- src/pages/NewOrderPage/NewOrderPage.jsx
+++ src/pages/NewOrderPage/NewOrderPage.jsx
@@ -1,5 +1,28 @@
+import { useState, useEffect } from 'react';
+import * as itemsAPI from '../../utilities/items-api';
+
 export default function NewOrderPage() {
+  // If your state will ultimately be an array, ALWAYS
+  // initialize to an empty array
+  const [menuItems, setMenuItems] = useState(42);
+
+  useEffect(function() {
+    async function getItems() {
+      const items = await itemsAPI.getAll();
+      setMenuItems(items);
+    }
+    getItems();
+    return function() {
+      console.log('going bye bye');
+    }
+  }, []);
+  // An empty dependency array results in the effect
+  // function running ONLY after the FIRST render
+  
   return (
+    <>
     <h1>NewOrderPage</h1>
+    <button onClick={() => setMenuItems(Date.now())}>Trigger re-render</button>
+    </>
   );
 }
